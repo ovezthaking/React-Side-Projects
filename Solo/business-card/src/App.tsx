4150d38a00afa76c802fc971a4d408e32ffadddd
Oliwer Urbaniak <oliwerx12@gmail.com>
2026-02-27 02:06:42 +0100

    Main Content component completed

--- Solo/business-card/src/App.tsx
+++ Solo/business-card/src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
 import Header from './components/Header'
+import MainContent from './components/MainContent'
 
 function App() {
   return (
     <>
       <Header />
+      <MainContent />
     </>
   )
 }
