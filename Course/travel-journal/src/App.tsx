84de3ff4aaf9c1e5c395b3e75c0d6241754f4dd4
Oliwer Urbaniak <oliwerx12@gmail.com>
2026-02-28 05:05:16 +0100

    hard coded Entry Component

--- Course/travel-journal/src/App.tsx
+++ Course/travel-journal/src/App.tsx
@@ -1,7 +1,14 @@
 import Header from "./components/Header";
+import Entry from "./components/Entry";
 
 export default function App() {
   return (
-    <Header />
+    <>
+      <Header />
+      <main className="container">
+        <Entry />
+      </main>
+    </>
+    
   )
 }
