14392c00f775c8ad05e4d92e9b8f75d0be1edd65
Oliwer Urbaniak <oliwerx12@gmail.com>
2026-03-01 04:27:16 +0100

    set up new project

--- Course/travel-journal/src/App.tsx
+++ Course/travel-journal/src/App.tsx
@@ -1,13 +1,14 @@
 import Header from "./components/Header";
 import Entry from "./components/Entry";
 import data from "./lib/data";
+import type { JSX } from "react";
 
 export default function App() {
   return (
     <>
       <Header />
       <main className="container">
-        {data.map(item => {
+        {data.map((item):JSX.Element => {
           return (
             <Entry
             key={item.id}
