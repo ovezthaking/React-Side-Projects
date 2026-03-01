167b66af9f8535ef7ed3c460c6903bbcd0b92238
Oliwer Urbaniak <oliwerx12@gmail.com>
2026-03-01 04:17:46 +0100

    fixing - key prop

--- Course/travel-journal/src/App.tsx
+++ Course/travel-journal/src/App.tsx
@@ -10,6 +10,7 @@ export default function App() {
         {data.map(item => {
           return (
             <Entry
+            key={item.id}
               {...item}
             />
         )})}
