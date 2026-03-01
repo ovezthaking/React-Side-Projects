fd4db6b6f25ebdae3a36726df23726348039802d
Oliwer Urbaniak <oliwerx12@gmail.com>
2026-03-01 04:47:03 +0100

    styles

--- Course/chef-claude/src/components/Header.tsx
+++ Course/chef-claude/src/components/Header.tsx
@@ -1,7 +1,9 @@
+import chefClaudeLogo from '/chef-claude-icon.png'
+
 export default function Header() {
     return (
         <header>
-            <img src="./chef-claude-icon.png" alt="Chef Claude logo image" />
+            <img src={chefClaudeLogo} alt="Chef Claude logo image" />
             <h1>Chef Claude</h1>
         </header>
     )
