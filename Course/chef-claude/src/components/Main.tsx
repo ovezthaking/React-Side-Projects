c8ffec82469132fc88be69e92a8a8cc4aa6ecedc
Oliwer Urbaniak <oliwerx12@gmail.com>
2026-03-02 04:54:27 +0100

    adding ingredients to the array

--- Course/chef-claude/src/components/Main.tsx
+++ Course/chef-claude/src/components/Main.tsx
@@ -1,13 +1,26 @@
+import { useState } from "react"
+
 export default function Main() {
-    const ingredients = ["Chicken", "Oregano", "Tomatoes"]
+    const [ingredients, setIngredients] = useState<Array<string>>([])
     
+    const handleSubmit = (e: React.SubmitEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        const formData = new FormData(e.currentTarget)
+        const newIngredient = formData.get('ingredient')
+        if(newIngredient && typeof newIngredient === 'string') {
+            setIngredients(prevIng => [...prevIng, newIngredient])
+        }
+        console.log(ingredients)
+    }
+
     return (
         <main>
-            <form className="add-ingredient-form">
+            <form className="add-ingredient-form" onSubmit={handleSubmit}>
                 <input 
                     type="text"
                     placeholder="e.g. oregano"
                     aria-label="Add ingredient"
+                    name="ingredient"
                 />
                 <button>Add ingredient</button>
             </form>
