36ce78380f180c5f17e9ca4fe5fab3adf06955ea
Oliwer Urbaniak <oliwerx12@gmail.com>
2026-03-03 04:11:57 +0100

    render get receipt div if there are more than 3 ingredients in the table in order to improve AI results

--- Course/chef-claude/src/components/Main.tsx
+++ Course/chef-claude/src/components/Main.tsx
@@ -29,13 +29,13 @@ export default function Main() {
                         <li key={ingredient}>{ingredient}</li>
                     ))}
                 </ul>
-                <div className="get-recipe-container">
+                {ingredients.length > 3 && <div className="get-recipe-container">
                     <div>
                         <h3>Ready for a recipe?</h3>
                         <p>Generate a recipe from your list of ingredients.</p>
                     </div>
                     <button>Get a recipe</button>
-                </div>
+                </div>}
             </section>}
         </main>
     )
