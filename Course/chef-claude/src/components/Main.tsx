43f4e9574c2826bf68827b84f82bd31e4a22621e
Oliwer Urbaniak <oliwerx12@gmail.com>
2026-03-03 04:07:05 +0100

    add new recipe section

--- Course/chef-claude/src/components/Main.tsx
+++ Course/chef-claude/src/components/Main.tsx
@@ -22,11 +22,21 @@ export default function Main() {
                 />
                 <button>Add ingredient</button>
             </form>
-            <ul>
-                {ingredients.map(ingredient => (
-                    <li key={ingredient}>{ingredient}</li>
-                ))}
-            </ul>
+            <section>
+                <h2>Ingredients on hand:</h2>
+                <ul className="ingredients-list" aria-live="polite">
+                    {ingredients.map(ingredient => (
+                        <li key={ingredient}>{ingredient}</li>
+                    ))}
+                </ul>
+                <div className="get-recipe-container">
+                    <div>
+                        <h3>Ready for a recipe?</h3>
+                        <p>Generate a recipe from your list of ingredients.</p>
+                    </div>
+                    <button>Get a recipe</button>
+                </div>
+            </section>
         </main>
     )
 }
