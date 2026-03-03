18761463571da871f5dfe1b2595a93576198c0c0
Oliwer Urbaniak <oliwerx12@gmail.com>
2026-03-03 04:09:05 +0100

    conditionally render section

--- Course/chef-claude/src/components/Main.tsx
+++ Course/chef-claude/src/components/Main.tsx
@@ -22,7 +22,7 @@ export default function Main() {
                 />
                 <button>Add ingredient</button>
             </form>
-            <section>
+            {ingredients.length > 0 && <section>
                 <h2>Ingredients on hand:</h2>
                 <ul className="ingredients-list" aria-live="polite">
                     {ingredients.map(ingredient => (
@@ -36,7 +36,7 @@ export default function Main() {
                     </div>
                     <button>Get a recipe</button>
                 </div>
-            </section>
+            </section>}
         </main>
     )
 }
