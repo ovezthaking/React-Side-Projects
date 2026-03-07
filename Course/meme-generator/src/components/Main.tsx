e5d2df4d0996bd832604b38695eb088979b1678c
Oliwer Urbaniak <oliwerx12@gmail.com>
2026-03-07 03:44:49 +0100

    add state to the component and make it controlled

--- Course/meme-generator/src/components/Main.tsx
+++ Course/meme-generator/src/components/Main.tsx
@@ -1,4 +1,23 @@
+import { useState } from "react"
+
 export default function Main() {
+    const memeObj = {
+        imageUrl: "http://i.imgflip.com/1bij.jpg",
+        topText: "One does not simply",
+        bottomText: "Walk into Mordor",
+    }
+
+    const [meme, setMeme] = useState(memeObj)
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const {value, name} = e.currentTarget
+
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            [name]: value
+        }))
+    }
+
     return (
         <main>
             <div className="form">
@@ -7,6 +26,8 @@ export default function Main() {
                         type="text"
                         placeholder="One does not simply"
                         name="topText"
+                        onChange={handleChange}
+                        value={meme.topText}
                     />
                 </label>
 
@@ -15,14 +36,16 @@ export default function Main() {
                         type="text"
                         placeholder="Walk into Mordor"
                         name="bottomText"
+                        onChange={handleChange}
+                        value={meme.bottomText}
                     />
                 </label>
                 <button>Get a new meme image 🖼</button>
             </div>
             <div className="meme">
-                <img src="http://i.imgflip.com/1bij.jpg" />
-                <span className="top">One does not simply</span>
-                <span className="bottom">Walk into Mordor</span>
+                <img src={meme.imageUrl} />
+                <span className="top">{meme.topText}</span>
+                <span className="bottom">{meme.bottomText}</span>
             </div>
         </main>
     )
