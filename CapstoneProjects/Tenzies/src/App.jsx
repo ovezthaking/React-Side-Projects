e160eb0ac52a3ce3cf6ea63e014fd882c04fcb89
Oliwer Urbaniak <oliwerx12@gmail.com>
2026-03-10 03:26:37 +0100

    refactor array of values to the array of objects

--- CapstoneProjects/Tenzies/src/App.jsx
+++ CapstoneProjects/Tenzies/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import Die from "./components/Die"
+import nanoid from 'nanoid'
 
 function App() {
   const [dice, setDice] = useState(generateAllNewDice())
@@ -8,15 +9,21 @@ function App() {
 
     return new Array(10)
       .fill(0)
-      .map(() => Math.ceil(Math.random() * 6))
+      .map(() => (
+        {
+          value: Math.ceil(Math.random() * 6),
+          isHeld: false,
+          id: nanoid()
+        }
+      ))
   }
 
   const rollDice = () => {
     setDice(generateAllNewDice())
   }
   
-  const diceElements = dice.map(value => (
-    <Die value={value}/>
+  const diceElements = dice.map(obj => (
+    <Die key={obj.id} value={obj.value}/>
   ))
 
   return (
