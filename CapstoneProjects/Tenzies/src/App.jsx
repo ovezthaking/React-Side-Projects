a640dbc5e2d0a3d83eef66b332fa19709a19ad17
Oliwer Urbaniak <oliwerx12@gmail.com>
2026-03-10 23:51:44 +0100

    Confetti on win

--- CapstoneProjects/Tenzies/src/App.jsx
+++ CapstoneProjects/Tenzies/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import Die from "./components/Die"
 import {nanoid} from 'nanoid'
+import Confetti from 'react-confetti'
 
 function App() {
   const [dice, setDice] = useState(generateAllNewDice())
@@ -41,8 +42,10 @@ function App() {
     />
   ))
 
+
   return (
     <main>
+      {gameWon && <Confetti/>}
       <h1 className="title">Tenzies</h1>
       <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
       <div className="dice-container">
@@ -50,7 +53,7 @@ function App() {
       </div>
       <button className="roll-dice" onClick={rollDice}>{gameWon ? 'New Game' : 'Roll'}</button>
     </main>
-    )
+  )
 }
 
 export default App
