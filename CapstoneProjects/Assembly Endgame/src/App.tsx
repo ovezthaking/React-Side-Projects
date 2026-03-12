af188114c2a878e1c5993a1f2896d329b2cc9584
Oliwer Urbaniak <oliwerx12@gmail.com>
2026-03-12 03:37:41 +0100

    game status section

--- CapstoneProjects/Assembly Endgame/src/App.tsx
+++ CapstoneProjects/Assembly Endgame/src/App.tsx
@@ -6,6 +6,10 @@ function App() {
         <h1>Assembly: Endgame</h1>
         <p>Guess the word in under 8 attempts to keep the programming world safe from Assembly!</p>
       </header>
+      <section className="game-status">
+        <h2>You win!</h2>
+        <p>Well done! 🎉</p>
+      </section>
     </main>
   )
 }
