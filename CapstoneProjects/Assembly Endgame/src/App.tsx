6d145060e2706cfec1882f7c438534c5ec07a2e5
Oliwer Urbaniak <oliwerx12@gmail.com>
2026-03-12 03:30:01 +0100

    header section

--- CapstoneProjects/Assembly Endgame/src/App.tsx
+++ CapstoneProjects/Assembly Endgame/src/App.tsx
@@ -1,8 +1,12 @@
 function App() {
 
   return (
-    <>
-    </>
+    <main>
+      <header>
+        <h1>Assembly: Endgame</h1>
+        <p>Guess the word in under 8 attempts to keep the programming world safe from Assembly!</p>
+      </header>
+    </main>
   )
 }
 
