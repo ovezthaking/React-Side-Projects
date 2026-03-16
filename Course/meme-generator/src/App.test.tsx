1ea3642e1d0bf7ed122de1926aa9f550adaf269f
Oliwer Urbaniak <oliwerx12@gmail.com>
2026-03-17 00:50:17 +0100

    App tests -> user interactions

---
+++ Course/meme-generator/src/App.test.tsx
@@ -0,0 +1,30 @@
+import { test, expect, describe } from 'vitest'
+import { userEvent } from '@testing-library/user-event'
+import { render, screen } from '@testing-library/react'
+import App from './App'
+
+describe('App', () => {
+    test('Updates the top text', async () => {
+        const user = userEvent.setup()
+        render(<App />)
+
+        const topTextbox = screen.getAllByRole('textbox')[0]
+
+        await user.clear(topTextbox)
+        await user.type(topTextbox, 'A coder does not simply')
+
+        expect(screen.getByText('A coder does not simply')).toBeInTheDocument()
+    })
+
+    test('Updates the bottom text', async () => {
+        const user = userEvent.setup()
+        render(<App />)
+
+        const bottomTextbox = screen.getAllByRole('textbox')[1]
+
+        await user.clear(bottomTextbox)
+        await user.type(bottomTextbox, 'Code without coffee')
+
+        expect(screen.getByText('Code without coffee')).toBeInTheDocument()
+    })
+})
