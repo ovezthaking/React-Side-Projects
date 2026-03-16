08fcb9565ddded25d7e3e5ca3579082d0e4385e8
Oliwer Urbaniak <oliwerx12@gmail.com>
2026-03-16 02:28:17 +0100

    dynamic win/lost section

--- CapstoneProjects/Assembly Endgame/src/App.tsx
+++ CapstoneProjects/Assembly Endgame/src/App.tsx
@@ -68,15 +68,34 @@ function App() {
     )
   })
 
+  const gameStatusClass = clsx('game-status', {
+    won: isGameWon,
+    lost: isGameLost
+  })
+
   return (
     <main>
       <header>
         <h1>Assembly: Endgame</h1>
         <p>Guess the word in under 8 attempts to keep the programming world safe from Assembly!</p>
       </header>
-      <section className="game-status">
-        <h2>You win!</h2>
-        <p>Well done! 🎉</p>
+      <section className={gameStatusClass}>
+        {isGameOver ? (
+          isGameWon ? (
+            <>
+              <h2>You win!</h2>
+              <p>Well done! 🎉</p>
+            </>
+          ): (
+            <>
+              <h2>Game over!</h2>
+              <p>You lose! Better start learning Assembly 😭</p>
+            </>
+          )
+        ) : (
+          null
+        )
+        }
       </section>
       <section className="language-chips">
         {languageElements}
