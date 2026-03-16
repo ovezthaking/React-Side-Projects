326213f7a26fddcbb3e9a27a8b2a6eb10c847864
Oliwer Urbaniak <oliwerx12@gmail.com>
2026-03-17 00:38:36 +0100

    more Main component text

--- Course/meme-generator/src/components/Main.test.tsx
+++ Course/meme-generator/src/components/Main.test.tsx
@@ -3,7 +3,7 @@ import Main from './Main'
 import { render, screen } from '@testing-library/react'
 
 describe('Main', () => {
-    test('displays top and bottom text', () => {
+    test('displays top and bottom text on the image', () => {
         render(<Main />)
 
         expect(screen.getByText('One does not simply')).toBeInTheDocument()
@@ -16,5 +16,25 @@ describe('Main', () => {
         const meme = screen.getByRole('img') as HTMLImageElement
 
         expect(meme.src).toBe('https://i.imgflip.com/1bij.jpg')
+    })
+
+    test('displays input labels text', () => {
+        render(<Main />)
+
+        expect(screen.getByText('Top Text')).toBeInTheDocument()
+        expect(screen.getByText('Bottom Text')).toBeInTheDocument()
+    })
+
+    test('displays placeholder inputs text', () => {
+        render(<Main />)
+
+        expect(screen.getByPlaceholderText('One does not simply')).toBeInTheDocument()
+        expect(screen.getByPlaceholderText('Walk into Mordor')).toBeInTheDocument()
+    })
+
+    test('displays button text', () => {
+        render(<Main />)
+
+        expect(screen.getByRole('button').textContent).toBe('Get a new meme image 🖼')
     })
 })
