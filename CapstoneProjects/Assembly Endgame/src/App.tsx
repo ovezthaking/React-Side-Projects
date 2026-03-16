5981958a34d6824b3cf1a2fd59bee5f1fe649f00
Oliwer Urbaniak <oliwerx12@gmail.com>
2026-03-16 02:33:59 +0100

    helper fn to render game status

--- CapstoneProjects/Assembly Endgame/src/App.tsx
+++ CapstoneProjects/Assembly Endgame/src/App.tsx
@@ -73,6 +73,27 @@ function App() {
     lost: isGameLost
   })
 
+  const renderGameStatus = () => {
+    if (!isGameOver) return null
+
+    if (isGameWon) {
+      return (
+        <>
+          <h2>You win!</h2>
+          <p>Well done! 🎉</p>
+        </>
+      )
+    }
+    else {
+      return (
+        <>
+          <h2>Game over!</h2>
+          <p>You lose! Better start learning Assembly 😭</p>
+        </>
+      )
+    }
+  }
+
   return (
     <main>
       <header>
@@ -80,22 +101,7 @@ function App() {
         <p>Guess the word in under 8 attempts to keep the programming world safe from Assembly!</p>
       </header>
       <section className={gameStatusClass}>
-        {isGameOver ? (
-          isGameWon ? (
-            <>
-              <h2>You win!</h2>
-              <p>Well done! 🎉</p>
-            </>
-          ): (
-            <>
-              <h2>Game over!</h2>
-              <p>You lose! Better start learning Assembly 😭</p>
-            </>
-          )
-        ) : (
-          null
-        )
-        }
+        {renderGameStatus()}
       </section>
       <section className="language-chips">
         {languageElements}
