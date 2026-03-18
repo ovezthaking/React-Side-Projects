8ffcbb450f9ae1d6e7928dd469469eb201a9a8aa
Oliwer Urbaniak <oliwerx12@gmail.com>
2026-03-18 01:12:51 +0100

    Blobs decoration

--- Solo/quizzical/src/App.tsx
+++ Solo/quizzical/src/App.tsx
@@ -1,6 +1,11 @@
+import BlobBlue from "./components/BlobBlue"
+import BlobYellow from "./components/BlobYellow"
+
 function App() {
   return (
     <>
+      <BlobYellow className="absolute right-0 top-0" />
+      <BlobBlue className="absolute left-0 bottom-0"/>
     </>
   )
 }
