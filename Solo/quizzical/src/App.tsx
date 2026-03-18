66f5d7499e09407b0411ca4f330459c4f97f4329
Oliwer Urbaniak <oliwerx12@gmail.com>
2026-03-18 02:59:50 +0100

    stylized Blobs

--- Solo/quizzical/src/App.tsx
+++ Solo/quizzical/src/App.tsx
@@ -1,12 +1,23 @@
+import { useState } from "react"
 import BlobBlue from "./components/BlobBlue"
 import BlobYellow from "./components/BlobYellow"
 
 function App() {
+  const [screen, setScreen] = useState('start')
+
   return (
-    <>
-      <BlobYellow className="absolute right-0 top-0" />
-      <BlobBlue className="absolute left-0 bottom-0"/>
-    </>
+    <main className="relative overflow-hidden min-h-screen">
+      <BlobYellow className={
+        screen === 'start' ? 
+        "absolute right-0 top-0" :
+        "absolute -right-5 -top-5"
+      } />
+      <BlobBlue className={
+        screen === 'start' ?
+        "absolute left-0 bottom-0" :
+        "absolute -left-8 -bottom-8"
+      }/>
+    </main>
   )
 }
 
