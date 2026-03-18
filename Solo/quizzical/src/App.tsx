867bb53e5e9b4291cb6e089655884ff242437b23
Oliwer Urbaniak <oliwerx12@gmail.com>
2026-03-18 03:55:27 +0100

    add vars for button styles and started with QuizPage

--- Solo/quizzical/src/App.tsx
+++ Solo/quizzical/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react"
 import BlobBlue from "./components/BlobBlue"
 import BlobYellow from "./components/BlobYellow"
 import LandingPage from "./components/LandingPage"
+import QuizPage from "./components/QuizPage"
 
 function App() {
   const [screen, setScreen] = useState<string>('start')
@@ -12,17 +13,22 @@ function App() {
 
   return (
     <main className="relative overflow-hidden min-h-screen">
-      <LandingPage handleStart={handleStart}/>
+      {screen === 'start' && 
+        <LandingPage handleStart={handleStart}/>
+      }
+      {screen === 'quiz' &&
+        <QuizPage />
+      }
       <BlobYellow className={
         screen === 'start'
-          ? "absolute right-0 top-0 transition-all duration-500 ease-out"
-          : "absolute -right-5 -top-5 transition-all duration-500 ease-out"
+          ? "fixed right-0 top-0 transition-all duration-500 ease-out"
+          : "fixed -right-5 -top-5 transition-all duration-500 ease-out"
       } 
       />
       <BlobBlue className={
         screen === 'start' ?
-        "absolute left-0 bottom-0 transition-all duration-500 ease-out" :
-        "absolute -left-8 -bottom-8 transition-all duration-500 ease-out"
+        "fixed left-0 bottom-0 transition-all duration-500 ease-out" :
+        "fixed -left-8 -bottom-8 transition-all duration-500 ease-out"
       }
       />
     </main>
---
+++ Solo/quizzical/src/components/QuizPage.tsx
@@ -0,0 +1,7 @@
+export default function QuizPage() {
+    return (
+        <section className="flex justify-center">
+            <button className="btn-seconary">Check answers</button>
+        </section>
+    )
+}
