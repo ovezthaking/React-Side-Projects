ad37937844a6f93bba05f737998529f58c62aafa
Oliwer Urbaniak <oliwerx12@gmail.com>
2026-03-19 01:46:40 +0100

    styled and improved components

--- Solo/quizzical/src/components/Question.tsx
+++ Solo/quizzical/src/components/Question.tsx
@@ -1,24 +1,17 @@
 export default function Question() {
+
+    const test = ['hola', 'answer2', 'answer3', 'answer4']
+
     return (
-        <div className="w-full">
-            <h2 className="text-[#293264] font-heading font-bold text-xl">This is a question, please answer</h2>
-            <div>
-                <label htmlFor="answer1">
-                    Answer1
-                    <input type="radio" name="answer" id="answer1" className="hidden" />
-                </label>
-                <label htmlFor="answer2">
-                    Answer2
-                    <input type="radio" name="answer" id="answer2" className="hidden" />
-                </label>
-                <label htmlFor="answer3">
-                    Answer3
-                    <input type="radio" name="answer" id="answer3" className="hidden" />
-                </label>
-                <label htmlFor="answer4">
-                    Answer4
-                    <input type="radio" name="answer" id="answer4" className="hidden" />
-                </label>
+        <div className="w-full border-b-2 border-b-[#DBDEF0] my-5">
+            <h2 className="text-[#293264] font-heading font-bold text-xl mb-5">This is a question, please answer</h2>
+            <div className="flex gap-5 mb-5">
+                {test.map((ans, index) => (
+                    <label htmlFor={ans} className="answer hover:bg-[#889cfc]" key={index}>
+                        {ans}
+                        <input type="radio" name="answer" id={ans} className="hidden" />
+                    </label>
+                ))}
             </div>
         </div>
     )
--- Solo/quizzical/src/components/QuizPage.tsx
+++ Solo/quizzical/src/components/QuizPage.tsx
@@ -2,10 +2,15 @@ import Question from "./Question";
 
 export default function QuizPage() {
     return (
-        <section className="flex flex-col items-center w-5xl pt-12 px-10">
+        <section className="flex flex-col items-center w-5xl pt-12 px-10 md:w-3xl">
             <Question />
-
-            <button className="btn-seconary">Check answers</button>
+            <Question />
+            <Question />
+            <Question />
+            <Question />
+            <div className="mt-7">
+                <button className="btn-seconary">Check answers</button>
+            </div>
         </section>
     )
 }
