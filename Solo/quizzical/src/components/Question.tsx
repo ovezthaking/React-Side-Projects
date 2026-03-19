1a9ad8331d7004df6c6852a0717062af3720b2dd
Oliwer Urbaniak <oliwerx12@gmail.com>
2026-03-19 04:20:58 +0100

    styling and refactor to use button instead of radio

--- Solo/quizzical/src/components/Question.tsx
+++ Solo/quizzical/src/components/Question.tsx
@@ -1,4 +1,5 @@
 export default function Question() {
+    
 
     const test = ['hola', 'answer2', 'answer3', 'answer4']
 
@@ -7,10 +8,17 @@ export default function Question() {
             <h2 className="text-[#293264] font-heading font-bold text-xl mb-5">This is a question, please answer</h2>
             <div className="flex gap-5 mb-5">
                 {test.map((ans, index) => (
-                    <label htmlFor={ans} className="answer hover:bg-[#889cfc]" key={index}>
+                    // <label htmlFor={ans} className="answer hover:bg-[#889cfc]" key={index}>
+                    //     {ans}
+                    //     <input type="radio" name="answer" id={ans} value={ans} className="hidden" />
+                    // </label>
+                    <button
+                        key={index}
+                        value={ans}
+                        className="answer hover:bg-[#889cfc] disabled:opacity-50 disabled:cursor-not-allowed disabled:hover:bg-[#F5F7FB]"
+                    >
                         {ans}
-                        <input type="radio" name="answer" id={ans} className="hidden" />
-                    </label>
+                    </button>
                 ))}
             </div>
         </div>
