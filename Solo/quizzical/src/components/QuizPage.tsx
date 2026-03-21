62a181ea85b3fae70c14575926504159a626d927
Oliwer Urbaniak <oliwerx12@gmail.com>
2026-03-21 02:08:48 +0100

    loading spinner

--- Solo/quizzical/src/components/QuizPage.tsx
+++ Solo/quizzical/src/components/QuizPage.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import Question from "./Question";
 import { getQuestions } from "../utils/quizUtils";
 import type { questionType } from "../lib/types";
+import { Loader2 } from "lucide-react";
 
 export default function QuizPage() {
     const [questions, setQuestions] = useState<questionType[] | never[]>([])
@@ -14,13 +15,17 @@ export default function QuizPage() {
     }, [])
 
     return (
-        <section className="flex flex-col items-center w-5xl pt-12 px-10 md:w-3xl">
-            {questions.map((question: questionType, index: number) => (
-                <Question key={index} question={question.question} answers={question.answers} />
-            ))}
-            <div className="mt-7">
-                <button className="btn-seconary">Check answers</button>
-            </div>
-        </section>
+        <>
+            {questions.length > 0 && questions ?
+                <section className="flex flex-col items-center w-5xl pt-12 px-10 md:w-3xl">
+                    {questions.map((question: questionType, index: number) => (
+                        <Question key={index} question={question.question} answers={question.answers} />
+                    ))}
+                    <div className="mt-7">
+                        <button className="btn-seconary">Check answers</button>
+                    </div>
+                </section>
+            : <p className="absolute top-[50%] animate-spin"><Loader2 /></p>}
+        </>
     )
 }
